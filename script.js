9765dfedab3d39beb2fbc4303afa4eac2f2c2fcb
unknown <teteulima@outlook.com>
2023-08-16 13:03:36 -0300

    inicial of the project

---
+++ script.js
@@ -0,0 +1,6 @@
+var password = document.getElementById("password")
+var confirme = document.getElementById("confirmePassword")
+
+if (password !== confirme) {
+    window.alert("Senhas diferentes,verifique de novo!")
+}
